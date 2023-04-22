b074d021a5104f3c2df586af707a2ffc170b6fbf
Bruno <brunoromano@hotmail.it>
2023-04-22 12:13:53 +0200

    common attributes for the entities

--- src/main/java/com/service/taskreport/entity/AbstractEntity.java
+++ src/main/java/com/service/taskreport/entity/AbstractEntity.java
@@ -1,2 +1,30 @@
-package com.service.taskreport.entity;public class AbstractEntity {
+package com.service.taskreport.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+public abstract class AbstractEntity {
+  @Id @GeneratedValue private Integer id;
+
+  @CreationTimestamp
+  @Column(
+      name = "insert_date",
+      updatable = false,
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime insertDate;
+
+  @UpdateTimestamp
+  @Column(
+      name = "update_date",
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private LocalDateTime updateDate;
 }
