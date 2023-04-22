8427fa9fea321d57faf56b9942e21205eea9b601
Bruno <brunoromano@hotmail.it>
2023-04-22 16:31:14 +0200

    task step sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO task_execution_report(task_execution_id,task_id,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (1,1,'2023-01-01 00:00:00','2023-01-01 00:01:00',60,'',NOW(),NOW());
+
+INSERT INTO task_step_execution_report(task_step_execution_id,task_execution_id,step_name,status,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (1,1,'step_1','SUCCESS','2023-01-01 00:00:00','2023-01-01 00:01:00',60,'',NOW(),NOW());
+INSERT INTO task_step_execution_report(task_step_execution_id,task_execution_id,step_name,status,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (2,1,'step_2','FAILURE','2023-01-01 00:00:00','2023-01-01 00:01:00',60,'',NOW(),NOW());
