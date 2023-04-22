82968237b9b0cab908b7a3175ab929613152fc06
Bruno <brunoromano@hotmail.it>
2023-04-22 16:51:01 +0200

    response classes

--- src/main/java/com/service/taskreport/response/TaskExecutionReportResponse.java
+++ src/main/java/com/service/taskreport/response/TaskExecutionReportResponse.java
@@ -1,2 +1,13 @@
-package com.service.taskreport.response;public class TaskExecutionReportResponse {
+package com.service.taskreport.response;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class TaskExecutionReportResponse {
+  private Integer id;
+  private Integer taskId;
+  private List<TaskStepExecutionReportResponse> taskStepExecutionReports;
+  private Timestamp startDateTime;
+  private Integer executionTimeSeconds;
+  private String errorMessage;
 }
--- src/main/java/com/service/taskreport/response/TaskStepExecutionReportResponse.java
+++ src/main/java/com/service/taskreport/response/TaskStepExecutionReportResponse.java
@@ -1,2 +1,16 @@
-package com.service.taskreport.response;public class TaskStepExecutionReportResponse {
+package com.service.taskreport.response;
+
+import com.service.taskreport.enums.StatusEnum;
+
+import java.sql.Timestamp;
+
+public class TaskStepExecutionReportResponse {
+  private Integer id;
+  private Integer taskExecutionId;
+  private String stepName;
+  private StatusEnum status;
+  private Timestamp startDateTime;
+  private Timestamp endDateTime;
+  private Integer executionTimeSeconds;
+  private String errorMessage;
 }
