49f6d0ac9d1867151aa46a9a378d65f7cd0aa398
Bruno <brunoromano@hotmail.it>
2023-04-22 12:16:48 +0200

    first version of TaskExecutionReport

--- src/main/java/com/service/taskreport/entity/TaskExecutionReport.java
+++ src/main/java/com/service/taskreport/entity/TaskExecutionReport.java
@@ -2,28 +2,25 @@ package com.service.taskreport.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
 import java.util.List;
 
-@Entity
-public class TaskExecutionReport extends AbstractEntity {
+@Entity(name = TaskExecutionReport.TABLE_NAME)
+@Table(name = TaskExecutionReport.TABLE_NAME)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskExecutionReport extends AbstractExecution {
   public static final String TABLE_NAME = "task_execution_report";
 
   @Column(name = "task_id")
   private Integer taskId;
-
-  @Column(name = "star_date_time")
-  private Timestamp startDateTime;
-
-  @Column(name = "end_date_time")
-  private Timestamp endDateTime;
-
-  @Column(name = "execution_time_seconds")
-  private Integer executionTimeSeconds;
-
-  @Column(name = "error_message")
-  private String errorMessage;
 
   @Column(name = "task_step_execution_reports")
   private List<TaskStepExecutionReport> taskStepExecutionReports;
