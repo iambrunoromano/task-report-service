362ef690c31bc0de1d80ea0a02d2e916e4b449a2
Bruno <brunoromano@hotmail.it>
2023-04-22 16:11:52 +0200

    first insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO task_execution_report(task_execution_id,task_id,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (1,1,'2023-01-01 00:00:00','2023-01-01 00:01:00',60,'',NOW(),NOW());
