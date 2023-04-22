1fb250b26f8ed010c8e2ee17125fd9c23763dbe8
Bruno <brunoromano@hotmail.it>
2023-04-22 12:13:18 +0200

    status enum

--- src/main/java/com/service/taskreport/enums/StatusEnum.java
+++ src/main/java/com/service/taskreport/enums/StatusEnum.java
@@ -1,2 +1,17 @@
-package com.service.taskreport.enums;public class StatusEnum {
+package com.service.taskreport.enums;
+
+public enum StatusEnum {
+  SUCCESS("SUCCESS"),
+  FAILURE("FAILURE"),
+  RUNNING("RUNNING");
+
+  private final String value;
+
+  StatusEnum(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
 }
