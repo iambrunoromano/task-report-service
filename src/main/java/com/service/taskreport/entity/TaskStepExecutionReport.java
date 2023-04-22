a5a5c01706a9a5faa61d81f457ab0d98d4477502
Bruno <brunoromano@hotmail.it>
2023-04-22 12:17:03 +0200

    first version of TaskStepExecutionReport

--- src/main/java/com/service/taskreport/entity/TaskStepExecutionReport.java
+++ src/main/java/com/service/taskreport/entity/TaskStepExecutionReport.java
@@ -1,2 +1,29 @@
-package com.service.taskreport.entity;public class TaskStepExecutionReport {
+package com.service.taskreport.entity;
+
+import com.service.taskreport.enums.StatusEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = TaskStepExecutionReport.TABLE_NAME)
+@Table(name = TaskStepExecutionReport.TABLE_NAME)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskStepExecutionReport extends AbstractExecution {
+  public static final String TABLE_NAME = "task_step_execution_report";
+
+  @Column(name = "task_execution_id")
+  private Integer taskExecutionId;
+
+  @Column(name = "step_name")
+  private String stepName;
+
+  @Column(name = "status")
+  private StatusEnum status;
 }
