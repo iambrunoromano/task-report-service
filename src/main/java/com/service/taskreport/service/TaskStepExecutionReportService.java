d6407d555bee58151e289eabe810cedea71bef48
Bruno <brunoromano@hotmail.it>
2023-04-22 16:39:49 +0200

    first TaskStepExecutionReportService

--- src/main/java/com/service/taskreport/service/TaskStepExecutionReportService.java
+++ src/main/java/com/service/taskreport/service/TaskStepExecutionReportService.java
@@ -1,2 +1,24 @@
-package com.service.taskreport.service;public class TaskStepExecutionReportService {
+package com.service.taskreport.service;
+
+import com.service.taskreport.entity.TaskStepExecutionReport;
+import com.service.taskreport.repository.TaskStepExecutionReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskStepExecutionReportService {
+
+  private final TaskStepExecutionReportRepository taskStepExecutionReportRepository;
+
+  @Autowired
+  TaskStepExecutionReportService(
+      TaskStepExecutionReportRepository taskStepExecutionReportRepository) {
+    this.taskStepExecutionReportRepository = taskStepExecutionReportRepository;
+  }
+
+  public List<TaskStepExecutionReport> getAll() {
+    return taskStepExecutionReportRepository.findAll();
+  }
 }
