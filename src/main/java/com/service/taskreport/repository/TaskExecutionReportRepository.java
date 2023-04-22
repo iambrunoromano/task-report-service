1e8cddad6070774ade75e8d05a48025180ecc57f
Bruno <brunoromano@hotmail.it>
2023-04-22 17:24:10 +0200

    find by id method

--- src/main/java/com/service/taskreport/repository/TaskExecutionReportRepository.java
+++ src/main/java/com/service/taskreport/repository/TaskExecutionReportRepository.java
@@ -4,6 +4,9 @@ import com.service.taskreport.entity.TaskExecutionReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TaskExecutionReportRepository
-    extends JpaRepository<TaskExecutionReport, Integer> {}
+public interface TaskExecutionReportRepository extends JpaRepository<TaskExecutionReport, Integer> {
+  Optional<TaskExecutionReport> findById(Integer id);
+}
