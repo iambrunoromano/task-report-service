09ff83ae55d9ae6183c9eab292809dc93f554aa1
Bruno <brunoromano@hotmail.it>
2023-04-22 11:35:11 +0200

    first commit

---
+++ src/main/java/com/service/taskreport/TaskReportApplication.java
@@ -0,0 +1,13 @@
+package com.service.taskreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/taskreport/TaskReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.taskreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
