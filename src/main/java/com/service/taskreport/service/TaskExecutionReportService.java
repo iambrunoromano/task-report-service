e8469951e0dcefacf20c213f94a297de82d97fb7
Bruno <brunoromano@hotmail.it>
2023-04-22 17:24:45 +0200

    getById/save/delete methods

--- src/main/java/com/service/taskreport/service/TaskExecutionReportService.java
+++ src/main/java/com/service/taskreport/service/TaskExecutionReportService.java
@@ -1,11 +1,13 @@
 package com.service.taskreport.service;
 
 import com.service.taskreport.entity.TaskExecutionReport;
+import com.service.taskreport.exception.TaskExecutionReportNotFoundException;
 import com.service.taskreport.repository.TaskExecutionReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskExecutionReportService {
@@ -19,5 +21,24 @@ public class TaskExecutionReportService {
 
   public List<TaskExecutionReport> getAll() {
     return taskExecutionReportRepository.findAll();
+  }
+
+  public TaskExecutionReport getById(Integer id) throws TaskExecutionReportNotFoundException {
+    Optional<TaskExecutionReport> optionalTaskExecutionReport =
+        taskExecutionReportRepository.findById(id);
+    if (optionalTaskExecutionReport.isPresent()) {
+      return optionalTaskExecutionReport.get();
+    }
+    throw new TaskExecutionReportNotFoundException(
+        String.format("TaskExecutionReport for id [%s] not found", id));
+  }
+
+  public TaskExecutionReport save(TaskExecutionReport taskExecutionReport) {
+    return taskExecutionReportRepository.save(taskExecutionReport);
+  }
+
+  public void delete(Integer id) throws TaskExecutionReportNotFoundException {
+    TaskExecutionReport taskExecutionReport = getById(id);
+    taskExecutionReportRepository.delete(taskExecutionReport);
   }
 }
