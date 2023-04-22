260bab3f9d6d54474cdeb6116e5f92efd640a64f
Bruno <brunoromano@hotmail.it>
2023-04-22 16:33:21 +0200

    fixed fields for database interface

--- src/main/java/com/service/taskreport/entity/TaskExecutionReport.java
+++ src/main/java/com/service/taskreport/entity/TaskExecutionReport.java
@@ -1,13 +1,15 @@
 package com.service.taskreport.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity(name = TaskExecutionReport.TABLE_NAME)
@@ -16,12 +18,45 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TaskExecutionReport extends AbstractExecution {
+public class TaskExecutionReport {
   public static final String TABLE_NAME = "task_execution_report";
+
+  @Id
+  @GeneratedValue
+  @Column(name = "task_execution_id")
+  private Integer id;
 
   @Column(name = "task_id")
   private Integer taskId;
 
-  @Column(name = "task_step_execution_reports")
+  @OneToMany
+  @JoinColumn(name = "task_execution_id", referencedColumnName = "task_execution_id")
   private List<TaskStepExecutionReport> taskStepExecutionReports;
+
+  @Column(name = "start_date_time")
+  private Timestamp startDateTime;
+
+  @Column(name = "end_date_time")
+  private Timestamp endDateTime;
+
+  @Column(name = "execution_time_seconds")
+  private Integer executionTimeSeconds;
+
+  @Column(name = "error_message")
+  private String errorMessage;
+
+  @CreationTimestamp
+  @Column(
+      name = "insert_date",
+      updatable = false,
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime insertDate;
+
+  @UpdateTimestamp
+  @Column(
+      name = "update_date",
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private LocalDateTime updateDate;
 }
--- src/main/java/com/service/taskreport/entity/TaskStepExecutionReport.java
+++ src/main/java/com/service/taskreport/entity/TaskStepExecutionReport.java
@@ -6,6 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity(name = TaskStepExecutionReport.TABLE_NAME)
 @Table(name = TaskStepExecutionReport.TABLE_NAME)
@@ -13,12 +18,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TaskStepExecutionReport extends AbstractExecution {
+public class TaskStepExecutionReport {
   public static final String TABLE_NAME = "task_step_execution_report";
 
   @Id
   @GeneratedValue
-  @Column(name = "id")
+  @Column(name = "task_step_execution_id")
   private Integer id;
 
   @Column(name = "task_execution_id")
@@ -28,5 +33,33 @@ public class TaskStepExecutionReport extends AbstractExecution {
   private String stepName;
 
   @Column(name = "status")
+  @Enumerated(EnumType.STRING)
   private StatusEnum status;
+
+  @Column(name = "start_date_time")
+  private Timestamp startDateTime;
+
+  @Column(name = "end_date_time")
+  private Timestamp endDateTime;
+
+  @Column(name = "execution_time_seconds")
+  private Integer executionTimeSeconds;
+
+  @Column(name = "error_message")
+  private String errorMessage;
+
+  @CreationTimestamp
+  @Column(
+      name = "insert_date",
+      updatable = false,
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime insertDate;
+
+  @UpdateTimestamp
+  @Column(
+      name = "update_date",
+      insertable = false,
+      columnDefinition = " DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private LocalDateTime updateDate;
 }
