29d1ae0ddf1d8c109edb5ba191a6a9045c6d368d
Bruno <brunoromano@hotmail.it>
2023-04-23 23:56:49 +0200

    new test files

--- src/test/resources/task/get_by_status_task_not_found.sql
+++ src/test/resources/task/get_by_status_task_not_found.sql
@@ -1,7 +1,2 @@
 TRUNCATE TABLE task_execution_report;
-INSERT INTO task_execution_report(task_execution_id,task_id,start_date_time,end_date_time,execution_time_seconds,error_message,status,insert_date,update_date) VALUES (3,3,'2023-01-01 00:00:00','2023-01-01 00:01:00',60,'','FAILURE',NOW(),NOW());
-INSERT INTO task_execution_report(task_execution_id,task_id,start_date_time,end_date_time,execution_time_seconds,error_message,status,insert_date,update_date) VALUES (4,4,'2023-01-01 00:00:00','2023-01-01 00:00:59',59,'','SUCCESS',NOW(),NOW());
-
 TRUNCATE TABLE task_step_execution_report;
-INSERT INTO task_step_execution_report(task_step_execution_id,task_execution_id,step_name,status,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (3,3,'step_1','SUCCESS','2023-01-01 00:00:00','2023-01-01 00:01:00',60,'',NOW(),NOW());
-INSERT INTO task_step_execution_report(task_step_execution_id,task_execution_id,step_name,status,start_date_time,end_date_time,execution_time_seconds,error_message,insert_date,update_date) VALUES (4,4,'step_1','SUCCESS','2023-01-01 00:00:00','2023-01-01 00:00:59',59,'',NOW(),NOW());
