d7a6cdd89c5354f72a6b5dc72829532da333097d
Bruno <brunoromano@hotmail.it>
2023-04-23 23:18:06 +0200

    get all test

--- src/test/java/com/service/taskreport/rest/TaskExecutionReportControllerTest.java
+++ src/test/java/com/service/taskreport/rest/TaskExecutionReportControllerTest.java
@@ -1,7 +1,34 @@
 package com.service.taskreport.rest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.service.taskreport.TestUtility;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
-class TaskExecutionReportControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class TaskExecutionReportControllerTest extends TestUtility {
+
+  private final TaskExecutionReportController taskExecutionReportController;
+
+  @Autowired
+  TaskExecutionReportControllerTest(TaskExecutionReportController taskExecutionReportController) {
+    this.taskExecutionReportController = taskExecutionReportController;
+  }
+
+  @Test
+  @Sql("classpath:task_get_all.sql")
+  void getAllTest() {
+    ResponseEntity actualResponse = taskExecutionReportController.getAll();
+    assertEquals(buildGetAllTaskResponses(), actualResponse.getBody());
+    assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+  }
 }
