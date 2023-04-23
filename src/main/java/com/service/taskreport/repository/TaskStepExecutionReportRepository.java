69e4e9579d58c34f1f8fed254f68ef1ce9980aa4
Bruno <brunoromano@hotmail.it>
2023-04-23 15:26:18 +0200

    New method to find by taskExecutionId sorting

--- src/main/java/com/service/taskreport/repository/TaskStepExecutionReportRepository.java
+++ src/main/java/com/service/taskreport/repository/TaskStepExecutionReportRepository.java
@@ -1,6 +1,7 @@
 package com.service.taskreport.repository;
 
 import com.service.taskreport.entity.TaskStepExecutionReport;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,8 @@ import java.util.Optional;
 public interface TaskStepExecutionReportRepository
     extends JpaRepository<TaskStepExecutionReport, Integer> {
   Optional<TaskStepExecutionReport> findById(Integer id);
+
+  List<TaskStepExecutionReport> findByTaskExecutionId(Integer taskExecutionId, Sort sort);
 
   List<TaskStepExecutionReport> findByTaskExecutionId(Integer taskExecutionId);
 }
