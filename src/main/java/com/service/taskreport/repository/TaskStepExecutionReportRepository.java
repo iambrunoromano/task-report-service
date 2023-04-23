f3ea7219d467063583102a47b40fca1520ed6da7
Bruno <brunoromano@hotmail.it>
2023-04-23 13:54:52 +0200

    Method to find step by id

--- src/main/java/com/service/taskreport/repository/TaskStepExecutionReportRepository.java
+++ src/main/java/com/service/taskreport/repository/TaskStepExecutionReportRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskStepExecutionReportRepository
     extends JpaRepository<TaskStepExecutionReport, Integer> {
+  Optional<TaskStepExecutionReport> findById(Integer id);
+
   List<TaskStepExecutionReport> findByTaskExecutionId(Integer taskExecutionId);
 }
