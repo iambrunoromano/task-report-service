83aaf67f8e600d8dacca5c93e92aa15015977d7e
Bruno <brunoromano@hotmail.it>
2023-04-23 15:48:37 +0200

    all sortable columns

--- src/main/java/com/service/taskreport/enums/TaskStepExecutionReportColumnNameEnum.java
+++ src/main/java/com/service/taskreport/enums/TaskStepExecutionReportColumnNameEnum.java
@@ -1,2 +1,24 @@
-package com.service.taskreport.enums;public enum TaskStepExecutionReportColumnNameEnum {
+package com.service.taskreport.enums;
+
+public enum TaskStepExecutionReportColumnNameEnum {
+  ID("id"),
+  TASK_EXECUTION_ID("taskExecutionId"),
+  STEP_NAME("stepName"),
+  STATUS("status"),
+  START_DATE_TIME("startDateTime"),
+  END_DATE_TIME("endDateTime"),
+  EXECUTION_TIME_SECONDS("executionTimeSeconds"),
+  ERROR_MESSAGE("errorMessage"),
+  INSERT_DATE("insertDate"),
+  UPDATE_DATE("updateDate");
+
+  private final String value;
+
+  TaskStepExecutionReportColumnNameEnum(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
 }
