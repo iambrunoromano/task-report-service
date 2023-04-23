fb4b4e7dd8f79d52eef7d2e45abf97b09ff31064
Bruno <brunoromano@hotmail.it>
2023-04-23 13:13:23 +0200

    new method to find steps by TaskExecutionReportList

--- src/main/java/com/service/taskreport/service/TaskStepExecutionReportService.java
+++ src/main/java/com/service/taskreport/service/TaskStepExecutionReportService.java
@@ -1,12 +1,16 @@
 package com.service.taskreport.service;
 
+import com.service.taskreport.entity.TaskExecutionReport;
 import com.service.taskreport.entity.TaskStepExecutionReport;
+import com.service.taskreport.exception.TaskStepExecutionReportBadRequestException;
 import com.service.taskreport.exception.TaskStepExecutionReportNotFoundException;
 import com.service.taskreport.repository.TaskStepExecutionReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskStepExecutionReportService {
@@ -32,6 +36,39 @@ public class TaskStepExecutionReportService {
           String.format(
               "TaskStepExecutionReport for taskExecutionId [%s] not found", taskExecutionId));
     }
+    return taskStepExecutionReportList;
+  }
+
+  public List<TaskStepExecutionReport> getByTaskExecutionReportList(
+      List<TaskExecutionReport> taskExecutionReportList)
+      throws TaskStepExecutionReportNotFoundException, TaskStepExecutionReportBadRequestException {
+    if (taskExecutionReportList.isEmpty()) {
+      throw new TaskStepExecutionReportBadRequestException("TaskExecutionReport empty list");
+    }
+    List<TaskStepExecutionReport> taskStepExecutionReportList =
+        getByTaskExecutionReports(taskExecutionReportList);
+    if (taskStepExecutionReportList.isEmpty()) {
+      List<Integer> idList =
+          taskExecutionReportList.stream()
+              .map(taskExecutionReport -> taskExecutionReport.getTaskId())
+              .collect(Collectors.toList());
+      throw new TaskStepExecutionReportNotFoundException(
+          String.format(
+              "TaskStepExecutionReport for taskExecutionReportList with ids [%s] not found",
+              idList));
+    }
+    return taskStepExecutionReportList;
+  }
+
+  private List<TaskStepExecutionReport> getByTaskExecutionReports(
+      List<TaskExecutionReport> taskExecutionReportList) {
+    List<TaskStepExecutionReport> taskStepExecutionReportList = new ArrayList<>();
+    taskExecutionReportList.stream()
+        .map(
+            taskExecutionReport ->
+                taskStepExecutionReportList.addAll(
+                    taskStepExecutionReportRepository.findByTaskExecutionId(
+                        taskExecutionReport.getId())));
     return taskStepExecutionReportList;
   }
 }
