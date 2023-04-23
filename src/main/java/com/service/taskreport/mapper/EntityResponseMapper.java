731f2ee89f5f1a18f2dc1fa5adb5c41cac7ea7d1
Bruno <brunoromano@hotmail.it>
2023-04-23 09:31:00 +0200

    list mapping methods

--- src/main/java/com/service/taskreport/mapper/EntityResponseMapper.java
+++ src/main/java/com/service/taskreport/mapper/EntityResponseMapper.java
@@ -6,11 +6,21 @@ import com.service.taskreport.response.TaskExecutionReportResponse;
 import com.service.taskreport.response.TaskStepExecutionReportResponse;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EntityResponseMapper {
-  TaskExecutionReportResponse taskExecutionReportToTaskExecutionReportResponse(
-      TaskExecutionReport taskExecutionReport);
 
   TaskStepExecutionReportResponse taskStepExecutionReportToTaskStepExecutionReportResponse(
       TaskStepExecutionReport taskStepExecutionReport);
+
+  List<TaskStepExecutionReportResponse>
+      taskStepExecutionReportListToTaskStepExecutionReportResponseList(
+          List<TaskStepExecutionReport> taskStepExecutionReportList);
+
+  TaskExecutionReportResponse taskExecutionReportToTaskExecutionReportResponse(
+      TaskExecutionReport taskExecutionReport);
+
+  List<TaskExecutionReportResponse> taskExecutionReportListToTaskExecutionReportResponseList(
+      List<TaskExecutionReport> taskExecutionReportList);
 }
