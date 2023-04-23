3c09df767444ffdfec159791228680062952e6d2
Bruno <brunoromano@hotmail.it>
2023-04-23 13:08:06 +0200

    now mapping happends taking in input step lists related to the task

--- src/main/java/com/service/taskreport/mapper/EntityResponseMapper.java
+++ src/main/java/com/service/taskreport/mapper/EntityResponseMapper.java
@@ -5,8 +5,11 @@ import com.service.taskreport.entity.TaskStepExecutionReport;
 import com.service.taskreport.response.TaskExecutionReportResponse;
 import com.service.taskreport.response.TaskStepExecutionReportResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface EntityResponseMapper {
@@ -18,9 +21,29 @@ public interface EntityResponseMapper {
       taskStepExecutionReportListToTaskStepExecutionReportResponseList(
           List<TaskStepExecutionReport> taskStepExecutionReportList);
 
+  @Mapping(target = "taskStepExecutionReports", source = "taskStepExecutionReportList")
   TaskExecutionReportResponse taskExecutionReportToTaskExecutionReportResponse(
-      TaskExecutionReport taskExecutionReport);
+      TaskExecutionReport taskExecutionReport,
+      List<TaskStepExecutionReport> taskStepExecutionReportList);
 
-  List<TaskExecutionReportResponse> taskExecutionReportListToTaskExecutionReportResponseList(
-      List<TaskExecutionReport> taskExecutionReportList);
+  default List<TaskExecutionReportResponse>
+      taskExecutionReportListToTaskExecutionReportResponseList(
+          List<TaskExecutionReport> taskExecutionReportList,
+          List<TaskStepExecutionReport> taskStepExecutionReportList) {
+    List<TaskExecutionReportResponse> taskExecutionReportResponseList = new ArrayList<>();
+    for (TaskExecutionReport taskExecutionReport : taskExecutionReportList) {
+      List<TaskStepExecutionReport> filteredTaskStepExecutionReport =
+          taskStepExecutionReportList.stream()
+              .filter(
+                  taskStepExecutionReport ->
+                      taskExecutionReport
+                          .getId()
+                          .equals(taskStepExecutionReport.getTaskExecutionId()))
+              .collect(Collectors.toList());
+      taskExecutionReportResponseList.add(
+          taskExecutionReportToTaskExecutionReportResponse(
+              taskExecutionReport, filteredTaskStepExecutionReport));
+    }
+    return taskExecutionReportResponseList;
+  }
 }
