5425b5cd5cdc467636542562b9e64618c4e7fd1f
Bruno <brunoromano@hotmail.it>
2023-04-23 13:06:02 +0200

    in case task steps are bad requested

---
+++ src/main/java/com/service/taskreport/exception/TaskStepExecutionReportBadRequestException.java
@@ -0,0 +1,7 @@
+package com.service.taskreport.exception;
+
+public class TaskStepExecutionReportBadRequestException extends BadRequestException {
+  public TaskStepExecutionReportBadRequestException(String message) {
+    super(message);
+  }
+}
