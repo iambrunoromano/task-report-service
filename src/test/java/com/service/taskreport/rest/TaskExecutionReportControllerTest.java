fd4410778c35d62edadcc15ee285a817a7eb60df
Bruno <brunoromano@hotmail.it>
2023-04-24 00:05:05 +0200

    getAllOrderByExecutionTime and getByStatus test

--- src/test/java/com/service/taskreport/rest/TaskExecutionReportControllerTest.java
+++ src/test/java/com/service/taskreport/rest/TaskExecutionReportControllerTest.java
@@ -1,6 +1,12 @@
 package com.service.taskreport.rest;
 
 import com.service.taskreport.TestUtility;
+import com.service.taskreport.enums.StatusEnum;
+import com.service.taskreport.exception.TaskExecutionReportNotFoundException;
+import com.service.taskreport.exception.TaskStepExecutionReportBadRequestException;
+import com.service.taskreport.exception.TaskStepExecutionReportNotFoundException;
+import com.service.taskreport.exception.UndefinedStatusException;
+import com.service.taskreport.response.TaskExecutionReportResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,7 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -25,10 +36,64 @@ class TaskExecutionReportControllerTest extends TestUtility {
   }
 
   @Test
-  @Sql("classpath:task_get_all.sql")
+  @Sql("classpath:task/get_all.sql")
   void getAllTest() {
-    ResponseEntity actualResponse = taskExecutionReportController.getAll();
+    ResponseEntity<List<TaskExecutionReportResponse>> actualResponse =
+        taskExecutionReportController.getAll();
     assertEquals(buildGetAllTaskResponses(), actualResponse.getBody());
+    assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+  }
+
+  @Test
+  @Sql("classpath:task/get_by_status.sql")
+  void getAllByStatusTest()
+      throws TaskExecutionReportNotFoundException, TaskStepExecutionReportBadRequestException,
+          TaskStepExecutionReportNotFoundException {
+    ResponseEntity<List<TaskExecutionReportResponse>> actualResponse =
+        taskExecutionReportController.getAllByStatus(StatusEnum.FAILURE);
+    assertEquals(
+        Arrays.asList(buildFirstTaskExecutionReportResponse(SECOND_ID, SECOND_TASK_ID)),
+        actualResponse.getBody());
+    assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+  }
+
+  @Test
+  @Sql("classpath:task/get_by_status_task_not_found.sql")
+  void getAllByStatusTaskNotFoundTest() {
+    TaskExecutionReportNotFoundException actualException =
+        assertThrows(
+            TaskExecutionReportNotFoundException.class,
+            () -> {
+              taskExecutionReportController.getAllByStatus(StatusEnum.FAILURE);
+            });
+    assertEquals(
+        String.format("TaskExecutionReport for status [%s] not found", StatusEnum.FAILURE),
+        actualException.getMessage());
+  }
+
+  @Test
+  @Sql("classpath:task/get_by_status_task_step_not_found.sql")
+  void getAllByStatusTaskStepNotFoundTest() {
+    TaskStepExecutionReportNotFoundException actualException =
+        assertThrows(
+            TaskStepExecutionReportNotFoundException.class,
+            () -> {
+              taskExecutionReportController.getAllByStatus(StatusEnum.FAILURE);
+            });
+    assertEquals(
+        String.format(
+            "TaskStepExecutionReport for taskExecutionReportList with ids [%s] not found",
+            Collections.singletonList(THIRD_TASK_ID)),
+        actualException.getMessage());
+  }
+
+  @Test
+  @Sql("classpath:task/get_all_order_by_execution_time.sql")
+  void getAllOrderByExecutionTimeTest()
+      throws TaskStepExecutionReportBadRequestException, TaskStepExecutionReportNotFoundException {
+    ResponseEntity<List<TaskExecutionReportResponse>> actualResponse =
+        taskExecutionReportController.getAllOrderByExecutionTime();
+    assertEquals(buildGetAllOrderByExecutionTime(), actualResponse.getBody());
     assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
   }
 }
