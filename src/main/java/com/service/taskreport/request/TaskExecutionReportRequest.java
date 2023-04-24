2672909055502a30dedcadda3263be5f937c522c
Bruno <brunoromano@hotmail.it>
2023-04-24 10:17:06 +0200

    builder

--- src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
@@ -1,15 +1,18 @@
 package com.service.taskreport.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
 import java.util.List;
 
 @Data
+@Builder
 public class TaskExecutionReportRequest {
   private Integer taskId;
   private List<TaskStepExecutionReportRequest> taskStepExecutionReports;
   private Timestamp startDateTime;
+  private Timestamp endDateTime;
   private Integer executionTimeSeconds;
   private String errorMessage;
 }
--- src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
@@ -1,11 +1,13 @@
 package com.service.taskreport.request;
 
 import com.service.taskreport.enums.StatusEnum;
+import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
 
 @Data
+@Builder
 public class TaskStepExecutionReportRequest {
   private Integer taskExecutionId;
   private String stepName;
