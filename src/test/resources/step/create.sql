d7d5c2c662664d4b888f8d513f15a93d15c23a3f
Bruno <brunoromano@hotmail.it>
2023-04-24 11:41:02 +0200

    create taskstep test files

---
+++ src/test/resources/step/create.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE task_execution_report;
+INSERT INTO task_execution_report(task_execution_id,task_id,start_date_time,end_date_time,execution_time_seconds,error_message,status,insert_date,update_date) VALUES (8,8,'2023-01-01 00:00:00','2023-01-01 00:01:00',60,'','FAILURE',NOW(),NOW());
+
+TRUNCATE TABLE task_step_execution_report;
