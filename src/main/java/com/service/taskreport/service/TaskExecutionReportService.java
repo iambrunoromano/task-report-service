6a734b8910a0a483fac5dd0eb2f4747472cc957a
Bruno <brunoromano@hotmail.it>
2023-04-24 23:09:09 +0200

    logs

--- src/main/java/com/service/taskreport/service/TaskExecutionReportService.java
+++ src/main/java/com/service/taskreport/service/TaskExecutionReportService.java
@@ -6,7 +6,7 @@ import com.service.taskreport.enums.StatusEnum;
 import com.service.taskreport.exception.TaskExecutionReportNotFoundException;
 import com.service.taskreport.exception.UndefinedStatusException;
 import com.service.taskreport.repository.TaskExecutionReportRepository;
-import com.service.taskreport.repository.TaskStepExecutionReportRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,31 +14,33 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class TaskExecutionReportService {
 
   private final TaskExecutionReportRepository taskExecutionReportRepository;
-  private final TaskStepExecutionReportRepository taskStepExecutionReportRepository;
   private final TaskStepExecutionReportService taskStepExecutionReportService;
 
   @Autowired
   TaskExecutionReportService(
       TaskExecutionReportRepository taskExecutionReportRepository,
-      TaskStepExecutionReportRepository taskStepExecutionReportRepository,
       TaskStepExecutionReportService taskStepExecutionReportService) {
     this.taskExecutionReportRepository = taskExecutionReportRepository;
-    this.taskStepExecutionReportRepository = taskStepExecutionReportRepository;
     this.taskStepExecutionReportService = taskStepExecutionReportService;
   }
 
   public List<TaskExecutionReport> getAll() {
-    return taskExecutionReportRepository.findAll();
+    List<TaskExecutionReport> taskExecutionReportList = taskExecutionReportRepository.findAll();
+    log.info("Found [{}] taskExecutionReports", taskExecutionReportList.size());
+    return taskExecutionReportList;
   }
 
   public TaskExecutionReport getById(Integer id) throws TaskExecutionReportNotFoundException {
     Optional<TaskExecutionReport> optionalTaskExecutionReport =
         taskExecutionReportRepository.findById(id);
     if (optionalTaskExecutionReport.isPresent()) {
-      return optionalTaskExecutionReport.get();
+      TaskExecutionReport taskExecutionReport = optionalTaskExecutionReport.get();
+      log.info("Found one taskExecutionReport for id [{}]: [{}]", id, taskExecutionReport);
+      return taskExecutionReport;
     }
     throw new TaskExecutionReportNotFoundException(
         String.format("TaskExecutionReport for id [%s] not found", id));
@@ -52,11 +54,12 @@ public class TaskExecutionReportService {
         setTaskExecutionReportStatus(taskExecutionReport, taskStepExecutionReportList);
     save(taskExecutionReport);
     if (!taskStepExecutionReportList.isEmpty()) {
+      log.info(
+          "Saving taskStepExecutionReports for taskExecutionReport with id [{}]",
+          taskExecutionReport.getId());
       for (TaskStepExecutionReport taskStepExecutionReport : taskStepExecutionReportList) {
         taskStepExecutionReport.setTaskExecutionId(taskExecutionReport.getId());
-        taskStepExecutionReport =
-            taskStepExecutionReportService.computeSeconds(taskStepExecutionReport);
-        taskStepExecutionReportRepository.save(taskStepExecutionReport);
+        taskStepExecutionReportService.save(taskStepExecutionReport);
       }
     }
     return taskExecutionReport;
@@ -64,16 +67,28 @@ public class TaskExecutionReportService {
 
   public void save(TaskExecutionReport taskExecutionReport) {
     taskExecutionReport = computeSeconds(taskExecutionReport);
-    taskExecutionReportRepository.save(taskExecutionReport);
+    taskExecutionReport = taskExecutionReportRepository.save(taskExecutionReport);
+    log.info(
+        "Saved taskExecutionReport with taskId [{}] - id [{}]",
+        taskExecutionReport.getTaskId(),
+        taskExecutionReport.getId());
   }
 
   public TaskExecutionReport computeSeconds(TaskExecutionReport taskExecutionReport) {
+    log.info(
+        "Computing executionTimeSeconds for taskExecutionReport with taskId [{}]",
+        taskExecutionReport.getTaskId());
     if (taskExecutionReport.getEndDateTime() != null
         && taskExecutionReport.getStartDateTime() != null) {
       long difference =
           taskExecutionReport.getEndDateTime().getTime()
               - taskExecutionReport.getStartDateTime().getTime();
-      taskExecutionReport.setExecutionTimeSeconds(Math.round(difference / 1000));
+      int computedSeconds = Math.round(difference / 1000);
+      log.info(
+          "Computed executionTimeSeconds as [{}] for taskExecutionReport with taskId [{}]",
+          computedSeconds,
+          taskExecutionReport.getTaskId());
+      taskExecutionReport.setExecutionTimeSeconds(computedSeconds);
     }
     return taskExecutionReport;
   }
@@ -92,16 +107,20 @@ public class TaskExecutionReportService {
     boolean running = false;
     boolean failure = false;
     if (taskStepExecutionReportList.isEmpty()) {
+      log.info("TaskStepExecutionReportList is empty");
       success = false;
     }
     for (TaskStepExecutionReport taskStepExecutionReport : taskStepExecutionReportList) {
       if (!StatusEnum.SUCCESS.equals(taskStepExecutionReport.getStatus())) {
+        log.info("Not all taskStepExecutionReports are in status [{}]", StatusEnum.SUCCESS);
         success = false;
       }
       if (StatusEnum.RUNNING.equals(taskStepExecutionReport.getStatus())) {
+        log.info("One taskStepExecutionReport has been found in status [{}]", StatusEnum.RUNNING);
         running = true;
       }
       if (StatusEnum.FAILURE.equals(taskStepExecutionReport.getStatus())) {
+        log.info("One taskStepExecutionReport has been found in status [{}]", StatusEnum.FAILURE);
         failure = true;
       }
     }
@@ -112,6 +131,8 @@ public class TaskExecutionReportService {
       throws TaskExecutionReportNotFoundException {
     List<TaskExecutionReport> taskExecutionReportList =
         taskExecutionReportRepository.findByStatus(status);
+    log.info(
+        "Found [{}] taskExecutionReports for status [{}]", taskExecutionReportList.size(), status);
     if (taskExecutionReportList.isEmpty()) {
       throw new TaskExecutionReportNotFoundException(
           String.format("TaskExecutionReport for status [%s] not found", status.getValue()));
@@ -127,16 +148,13 @@ public class TaskExecutionReportService {
       TaskExecutionReport taskExecutionReport, boolean success, boolean running, boolean failure)
       throws UndefinedStatusException {
     if (running) {
-      taskExecutionReport.setStatus(StatusEnum.RUNNING);
-      return taskExecutionReport;
+      return logAndSetStatus(taskExecutionReport, StatusEnum.RUNNING);
     } else {
       if (failure) {
-        taskExecutionReport.setStatus(StatusEnum.FAILURE);
-        return taskExecutionReport;
+        return logAndSetStatus(taskExecutionReport, StatusEnum.FAILURE);
       }
       if (success) {
-        taskExecutionReport.setStatus(StatusEnum.SUCCESS);
-        return taskExecutionReport;
+        return logAndSetStatus(taskExecutionReport, StatusEnum.SUCCESS);
       }
     }
     throw new UndefinedStatusException(
@@ -145,8 +163,23 @@ public class TaskExecutionReportService {
             taskExecutionReport.getTaskId()));
   }
 
+  private TaskExecutionReport logAndSetStatus(
+      TaskExecutionReport taskExecutionReport, StatusEnum statusEnum) {
+    log.info(
+        "Setting taskExecutionReport status to [{}] for taskId [{}]",
+        statusEnum,
+        taskExecutionReport.getTaskId());
+    taskExecutionReport.setStatus(statusEnum);
+    return taskExecutionReport;
+  }
+
   public List<TaskExecutionReport> getMissingExecutionTime() {
-    return taskExecutionReportRepository
-        .findByStartDateTimeIsNotNullAndEndDateTimeIsNotNullAndExecutionTimeSecondsIsNull();
+    List<TaskExecutionReport> taskExecutionReportList =
+        taskExecutionReportRepository
+            .findByStartDateTimeIsNotNullAndEndDateTimeIsNotNullAndExecutionTimeSecondsIsNull();
+    log.info(
+        "Found [{}] taskExecutionReport with non-null startDateTime, non-null endDateTime, null executionTimeSeconds",
+        taskExecutionReportList.size());
+    return taskExecutionReportList;
   }
 }
