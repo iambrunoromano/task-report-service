3b70964dbd07d69ca292976efb219a196082fba9
Bruno <brunoromano@hotmail.it>
2023-04-24 23:20:49 +0200

    logs

--- src/main/java/com/service/taskreport/rest/TaskExecutionReportController.java
+++ src/main/java/com/service/taskreport/rest/TaskExecutionReportController.java
@@ -15,6 +15,7 @@ import com.service.taskreport.response.TaskExecutionReportResponse;
 import com.service.taskreport.service.TaskExecutionReportService;
 import com.service.taskreport.service.TaskStepExecutionReportService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "task-execution-reports")
+@Slf4j
 public class TaskExecutionReportController {
   private final TaskExecutionReportService taskExecutionReportService;
   private final TaskStepExecutionReportService taskStepExecutionReportService;
@@ -43,11 +45,13 @@ public class TaskExecutionReportController {
 
   @GetMapping
   public ResponseEntity<List<TaskExecutionReportResponse>> getAll() {
+    log.info("New [GET-ALL] request");
     List<TaskExecutionReport> taskExecutionReportList = taskExecutionReportService.getAll();
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         taskStepExecutionReportService.getAll();
     List<TaskExecutionReportResponse> taskExecutionReportResponseList =
         mapListToResponse(taskExecutionReportList, taskStepExecutionReportList);
+    log.info("Returning [{}] taskExecutionReports", taskExecutionReportResponseList.size());
     return ResponseEntity.ok(taskExecutionReportResponseList);
   }
 
@@ -56,35 +60,41 @@ public class TaskExecutionReportController {
       @PathVariable StatusEnum status)
       throws TaskExecutionReportNotFoundException, TaskStepExecutionReportBadRequestException,
           TaskStepExecutionReportNotFoundException {
+    log.info("New [GET-BY-STATUS] request");
     List<TaskExecutionReport> taskExecutionReportList =
         taskExecutionReportService.getByStatus(status);
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         taskStepExecutionReportService.getByTaskExecutionReportList(taskExecutionReportList);
     List<TaskExecutionReportResponse> taskExecutionReportResponseList =
         mapListToResponse(taskExecutionReportList, taskStepExecutionReportList);
+    log.info("Returning [{}] taskExecutionReports", taskExecutionReportResponseList.size());
     return ResponseEntity.ok(taskExecutionReportResponseList);
   }
 
   @GetMapping(value = "/execution-time")
   public ResponseEntity<List<TaskExecutionReportResponse>> getAllOrderByExecutionTime()
       throws TaskStepExecutionReportBadRequestException, TaskStepExecutionReportNotFoundException {
+    log.info("New [GET-ORDERED-BY-EXECUTION-TIME] request");
     List<TaskExecutionReport> taskExecutionReportList =
         taskExecutionReportService.getAllOrderByExecutionTimeSeconds();
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         taskStepExecutionReportService.getByTaskExecutionReportList(taskExecutionReportList);
     List<TaskExecutionReportResponse> taskExecutionReportResponseList =
         mapListToResponse(taskExecutionReportList, taskStepExecutionReportList);
+    log.info("Returning [{}] taskExecutionReports", taskExecutionReportResponseList.size());
     return ResponseEntity.ok(taskExecutionReportResponseList);
   }
 
   @GetMapping(value = "/{id}")
   public ResponseEntity<TaskExecutionReportResponse> getById(@PathVariable Integer id)
       throws TaskExecutionReportNotFoundException, TaskStepExecutionReportNotFoundException {
+    log.info("New [GET-BY-ID] request");
     TaskExecutionReport taskExecutionReport = taskExecutionReportService.getById(id);
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         taskStepExecutionReportService.getByTaskExecutionId(id);
     TaskExecutionReportResponse taskExecutionReportResponse =
         mapToResponse(taskExecutionReport, taskStepExecutionReportList);
+    log.info("Returning taskExecutionReportResponse [{}]", taskExecutionReportResponse);
     return ResponseEntity.ok(taskExecutionReportResponse);
   }
 
@@ -92,6 +102,7 @@ public class TaskExecutionReportController {
   public ResponseEntity<TaskExecutionReportResponse> create(
       @RequestBody @Valid TaskExecutionReportRequest taskExecutionReportRequest)
       throws UndefinedStatusException, TaskStepExecutionReportNotFoundException {
+    log.info("New [CREATE] request");
     TaskExecutionReport taskExecutionReport = mapToEntity(taskExecutionReportRequest);
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         mapListToEntity(taskExecutionReportRequest.getTaskStepExecutionReports());
@@ -101,14 +112,17 @@ public class TaskExecutionReportController {
         taskStepExecutionReportService.getByTaskExecutionId(taskExecutionReport.getId());
     TaskExecutionReportResponse taskExecutionReportResponse =
         mapToResponse(taskExecutionReport, taskStepExecutionReportList);
+    log.info("Returning taskExecutionReportResponse [{}]", taskExecutionReportResponse);
     return ResponseEntity.ok(taskExecutionReportResponse);
   }
 
   @DeleteMapping(value = "/{id}")
   public ResponseEntity<Void> delete(@PathVariable Integer id)
       throws TaskExecutionReportNotFoundException {
+    log.info("New [CREATE] request");
     taskExecutionReportService.delete(id);
     taskStepExecutionReportService.deleteByTaskExecutionId(id);
+    log.info("Returning 200 OK");
     return ResponseEntity.ok().build();
   }
 
