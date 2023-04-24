8feaf57437c318a020e2e008c231b68bc66eae1d
Bruno <brunoromano@hotmail.it>
2023-04-24 23:45:35 +0200

    back in ids to allow update

--- src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
@@ -10,11 +10,16 @@ import java.util.List;
 @Data
 @Builder
 public class TaskExecutionReportRequest {
+  private Integer id;
+
   @NotNull(message = "taskId should not be null")
   private Integer taskId;
+
   private List<TaskStepExecutionReportRequest> taskStepExecutionReports;
+
   @NotNull(message = "startDateTime should not be null")
   private Timestamp startDateTime;
+
   private Timestamp endDateTime;
   private String errorMessage;
 }
--- src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
@@ -10,13 +10,19 @@ import java.sql.Timestamp;
 @Data
 @Builder
 public class TaskStepExecutionReportRequest {
+  private Integer id;
+
   @NotNull(message = "taskExecutionId should not be null")
   private Integer taskExecutionId;
+
   @NotNull(message = "stepName should not be null")
   private String stepName;
+
   private StatusEnum status;
+
   @NotNull(message = "startDateTime should not be null")
   private Timestamp startDateTime;
+
   private Timestamp endDateTime;
   private String errorMessage;
 }
