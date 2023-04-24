7b2fa3b3b73bf4faec49b2c61e0dc842ad4e1496
Bruno <brunoromano@hotmail.it>
2023-04-24 15:48:57 +0200

    requests with validated fields

--- src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskExecutionReportRequest.java
@@ -1,5 +1,6 @@
 package com.service.taskreport.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +10,10 @@ import java.util.List;
 @Data
 @Builder
 public class TaskExecutionReportRequest {
+  @NotNull(message = "taskId should not be null")
   private Integer taskId;
   private List<TaskStepExecutionReportRequest> taskStepExecutionReports;
+  @NotNull(message = "startDateTime should not be null")
   private Timestamp startDateTime;
   private Timestamp endDateTime;
   private Integer executionTimeSeconds;
--- src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
+++ src/main/java/com/service/taskreport/request/TaskStepExecutionReportRequest.java
@@ -1,6 +1,7 @@
 package com.service.taskreport.request;
 
 import com.service.taskreport.enums.StatusEnum;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,9 +10,12 @@ import java.sql.Timestamp;
 @Data
 @Builder
 public class TaskStepExecutionReportRequest {
+  @NotNull(message = "taskExecutionId should not be null")
   private Integer taskExecutionId;
+  @NotNull(message = "stepName should not be null")
   private String stepName;
   private StatusEnum status;
+  @NotNull(message = "startDateTime should not be null")
   private Timestamp startDateTime;
   private Timestamp endDateTime;
   private Integer executionTimeSeconds;
