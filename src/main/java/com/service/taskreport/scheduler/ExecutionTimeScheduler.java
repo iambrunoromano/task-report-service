80bd6e0697c3ad7cde62edb66b950f76cf3c11b8
Bruno <brunoromano@hotmail.it>
2023-04-24 22:40:32 +0200

    logs

--- src/main/java/com/service/taskreport/scheduler/ExecutionTimeScheduler.java
+++ src/main/java/com/service/taskreport/scheduler/ExecutionTimeScheduler.java
@@ -2,18 +2,17 @@ package com.service.taskreport.scheduler;
 
 import com.service.taskreport.entity.TaskExecutionReport;
 import com.service.taskreport.entity.TaskStepExecutionReport;
-import com.service.taskreport.repository.TaskExecutionReportRepository;
-import com.service.taskreport.repository.TaskStepExecutionReportRepository;
 import com.service.taskreport.service.TaskExecutionReportService;
 import com.service.taskreport.service.TaskStepExecutionReportService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class ExecutionTimeScheduler {
 
   private final TaskExecutionReportService taskExecutionReportService;
@@ -29,15 +28,19 @@ public class ExecutionTimeScheduler {
 
   @Scheduled(cron = "${scheduled.execution-time.cron}")
   public void computeMissingExecutionTime() {
+    log.info("Start scheduled computeMissingExecutionTime job");
     List<TaskStepExecutionReport> taskStepExecutionReportList =
         taskStepExecutionReportService.getMissingExecutionTime();
+    log.info("Found [{}] taskStepExecutionReport to compute", taskStepExecutionReportList.size());
     for (TaskStepExecutionReport taskStepExecutionReport : taskStepExecutionReportList) {
       taskStepExecutionReportService.save(taskStepExecutionReport);
     }
     List<TaskExecutionReport> taskExecutionReportList =
         taskExecutionReportService.getMissingExecutionTime();
+    log.info("Found [{}] taskExecutionReport to compute", taskExecutionReportList.size());
     for (TaskExecutionReport taskExecutionReport : taskExecutionReportList) {
       taskExecutionReportService.save(taskExecutionReport);
     }
+    log.info("End scheduled computeMissingExecutionTime job");
   }
 }
