7694e9f18eeae6218d45cede58f604ddc43fb9fd
Bruno <brunoromano@hotmail.it>
2023-06-06 12:22:36 +0200

    persistence config

--- src/main/java/com/service/taskreport/config/PersistenceConfig.java
+++ src/main/java/com/service/taskreport/config/PersistenceConfig.java
@@ -1,2 +1,28 @@
-package com.service.taskreport;public class PersistenceConfig {
+package com.service.taskreport.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.service.taskreport.mapper.persistence")
+public class PersistenceConfig {
+
+  @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+  }
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    sqlSessionFactoryBean.setDataSource(dataSource());
+    return sqlSessionFactoryBean.getObject();
+  }
 }
