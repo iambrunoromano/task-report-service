5ec9bc35f3321e215358306cbffe64c521e56f4c
Bruno <brunoromano@hotmail.it>
2023-06-07 11:04:42 +0200

    works with xml configuration

--- src/main/java/com/service/taskreport/config/PersistenceConfig.java
+++ src/main/java/com/service/taskreport/config/PersistenceConfig.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -13,6 +14,8 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan("com.service.taskreport.mapper.persistence")
 public class PersistenceConfig {
+
+  private static final String MYBATIS_CONFIG = "mybatis-config.xml";
 
   @Bean
   public DataSource dataSource() {
@@ -23,6 +26,7 @@ public class PersistenceConfig {
   public SqlSessionFactory sqlSessionFactory() throws Exception {
     SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
     sqlSessionFactoryBean.setDataSource(dataSource());
+    sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG));
     return sqlSessionFactoryBean.getObject();
   }
 }
